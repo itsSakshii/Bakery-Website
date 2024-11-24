38af411367503e15484d18936d3df27cd2c7ca4a
itsSakshii <its.sakshii.me@gmail.com>
2024-11-24 17:17:45 +0530

    updated website

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+
+
+    return (
+      <>
+       <ul className="flex navbar">
+         <li className="navbar-item">Manufacturing</li>
+         <li className="navbar-item">Packaging</li>
+         <li className="navbar-item">Packaging</li>
+         <li className="navbar-item">Farms</li>
+         <li className="navbar-item">Delivery</li>
+
+        </ul> 
+      </>
+    )
+  }
+  
+  export default Navbar;
+  
